0567127a2d2c1fc31e3cea542b39d100c614b46c
nickt-t-go <nickt444@gmail.com>
2016-10-05 22:39:13 -0400

    Added a view

--- src/comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace comic_book_gallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller.");
+            return View();
         }
     }
 }
